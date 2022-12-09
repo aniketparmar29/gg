bd5b3985b2f1090d5ae1376107a592ba104e8e74
Akash Nandoliya <nandoliyaakas55@gmail.com>
2022-12-08 21:47:08 -0800

    sec

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import {Navbar} from "./Component/Navbar.js";
 let nav = document.getElementById("navbar");
 nav.innerHTML=Navbar();
+
+const ajv=  async ()=>{
+try{
+    let res = await fetch(`http://localhost:3000/products
+`);
+let data = await res.json();
+append(data);
+}catch(e){
+    console.log(e);
+}
+}
+ajv();
+const append = (data)=>{
+console.log(data)
+}
